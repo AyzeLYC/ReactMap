0ce939117710d369066b3498d969299bed78cbee
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-25 18:25:55 +0200

    Updated useMapStore.js

--- src/store/useMapStore.js
+++ src/store/useMapStore.js
@@ -1,9 +1,8 @@
 // @ts-check
-
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 /**
- * @typedef {{ map: import('leaflet').Map | null }} UseMapStore
- * @type {import("zustand").UseBoundStore<import("zustand").StoreApi<UseMapStore>>}
- */
-export const useMapStore = create(() => ({ map: null }))
+ * @typedef { { map: import( 'leaflet' ).Map | null } } UseMapStore
+ * @type { import( "zustand" ).UseBoundStore<import( "zustand" ).StoreApi< UseMapStore > >}
+**/
+export const useMapStore = create( () => ( { map: null } ) );
