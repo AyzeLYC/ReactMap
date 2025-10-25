be7e3b5eb72a739947171499e0a5bdd9d321b1e5
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-25 16:54:07 +0200

    Updated formatDistance.js

--- src/utils/formatDistance.js
+++ src/utils/formatDistance.js
@@ -1,21 +1,22 @@
 // @ts-check
-import { useStorage } from '@store/useStorage'
 
-const METERS_PER_MILE = 1609.344
+import { useStorage } from '@store/useStorage';
 
-export function formatDistance(
-  meters = 0,
-  unit = useStorage.getState().settings.distanceUnit,
-  locale = localStorage.getItem('i18nextLng') || 'en',
-) {
-  const distance = unit === 'miles' ? meters / METERS_PER_MILE : meters / 1000
+const METERS_PER_MILE = 1609.344;
+
+export function formatDistance( meters = 0, unit = useStorage.getState().settings.distanceUnit, locale = localStorage.getItem( 'i18nextLng' ) || 'en' ) {
+  
+  const distance = unit === 'miles' ? ( meters / METERS_PER_MILE ) : ( meters / 1000 );
 
   const numFormatter = new Intl.NumberFormat(locale, {
+    
     unitDisplay: 'short',
     unit: unit.replace(/s$/, ''),
     style: 'unit',
-    maximumFractionDigits: 2,
-  })
+    maximumFractionDigits: 2
+    
+  });
 
-  return numFormatter.format(distance)
-}
+  return numFormatter.format(distance);
+  
+};
