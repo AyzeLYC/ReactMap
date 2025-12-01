be234aa5ee10dbb56073049ea39547f6c4f11cab
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 20:07:00 +0100

    Updated setLoadingText.js

--- src/utils/setLoadingText.js
+++ src/utils/setLoadingText.js
@@ -1,9 +1,17 @@
 // @ts-check
+
 /**
  * Sets the non-reactive loading element's inner text
- * @param {string} text
- */
-export function setLoadingText(text) {
-  const loadingText = document.getElementById('loading-text')
-  if (loadingText) loadingText.innerText = text
-}
+ * @param { string } text
+**/
+export function setLoadingText( text ) {
+  
+  const loadingText = document.getElementById( 'loading-text' );
+  
+  if ( loadingText ) {
+    
+    loadingText.innerText = text;
+    
+  };
+  
+};
