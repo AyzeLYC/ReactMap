3a3f29dff4655431a5b1d3b2cbe395d31f5d118d
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 18:36:08 +0100

    Updated dayCheck.js

--- src/utils/dayCheck.js
+++ src/utils/dayCheck.js
@@ -3,11 +3,17 @@
 /**
  * @param {number} currentStamp
  * @param {number} desiredStamp
- */
+**/
 export function dayCheck(currentStamp, desiredStamp) {
-  const locale = localStorage.getItem('i18nextLng') || 'en'
+  
+  const locale = localStorage.getItem('i18nextLng') || 'en';
+  
   if (currentStamp - desiredStamp < 86400) {
-    return new Date(desiredStamp * 1000).toLocaleTimeString(locale)
-  }
-  return new Date(desiredStamp * 1000).toLocaleString(locale)
-}
+    
+    return new Date(desiredStamp * 1000).toLocaleTimeString(locale);
+    
+  };
+  
+  return new Date(desiredStamp * 1000).toLocaleString(locale);
+  
+};
