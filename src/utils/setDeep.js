f73035dee7f544c957389629d45ee07225dc465e
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 20:05:27 +0100

    Updated setDeep.js

--- src/utils/setDeep.js
+++ src/utils/setDeep.js
@@ -1,32 +1,40 @@
 // @ts-check
 /**
- *
- * @param {object} obj
- * @param {string | string[]} path
- * @param {any} value
- */
-export function setDeep(obj, path, value) {
-  if (typeof path === 'string') {
-    path = path.split('.')
-  }
-  if (path.length > 1) {
-    const next = path.shift()
+ * @param { object } obj
+ * @param { string | string[] } path
+ * @param { any } value
+**/
+export function setDeep( obj, path, value ) {
+  
+  if ( typeof path === 'string' ) {
+    
+    path = path.split( '.' );
+    
+  };
+  if ( path.length > 1 ) {
+    
+    const next = path.shift();
+    
     setDeep(
-      (obj[next] =
-        Object.prototype.toString.call(obj[next]) === '[object Object]'
-          ? { ...obj[next] }
-          : {}),
+      ( obj[ ( next ) ] =
+        Object.prototype.toString.call( obj[ ( next ) ] ) === '[object Object]'
+          ? { ...obj[ ( next ) ] }
+          : {} ),
       path,
-      value,
-    )
+      value
+    );
+    
   } else {
-    obj[path[0]] =
+    
+    obj[ ( path[ 0 ] ) ] =
       typeof value === 'object'
-        ? Array.isArray(value)
+        ? Array.isArray( value )
           ? value.slice()
           : { ...value }
-        : value
-  }
+        : value;
+    
+  };
 
-  return { ...obj }
-}
+  return { ...obj };
+  
+};
