622aee122018ab12cb0686b3ef7aeba848dd35ad
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 18:18:56 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // @ts-check
-import '@assets/css/main.css'
-import 'leaflet.locatecontrol/dist/L.Control.Locate.css'
-import 'leaflet/dist/leaflet.css'
+import '@assets/css/main.css';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
+import 'leaflet/dist/leaflet.css';
 
-import * as React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import CssBaseline from '@mui/material/CssBaseline'
-import ThemeProvider from '@mui/material/styles/ThemeProvider'
-import { ApolloProvider } from '@apollo/client'
+import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { ApolloProvider } from '@apollo/client';
 
-import { useCustomTheme } from '@assets/theme'
-import { globalStyles } from '@components/Global'
-import { apolloClient } from '@services/apollo'
-import { isLocalStorageEnabled } from '@utils/isLocalStorageEnabled'
-import { setLoadingText } from '@utils/setLoadingText'
-import '@services/events'
-import { ErrorBoundary } from '@components/ErrorBoundary'
-import { HolidayEffects } from '@features/holiday'
+import { useCustomTheme } from '@assets/theme';
+import { globalStyles } from '@components/Global';
+import { apolloClient } from '@services/apollo';
+import { isLocalStorageEnabled } from '@utils/isLocalStorageEnabled';
+import { setLoadingText } from '@utils/setLoadingText';
+import '@services/events';
+import { ErrorBoundary } from '@components/ErrorBoundary';
+import { HolidayEffects } from '@features/holiday';
 
-import { Pages } from './pages'
+import { Pages } from './pages';
 
 const LOADING_LOCALES = {
   de: 'Übersetzungen werden geladen',
@@ -35,23 +35,29 @@ const LOADING_LOCALES = {
   sv: 'Laddar Översättningar',
   th: 'กำลังโหลดการแปล',
   tr: 'Çeviriler Yükleniyor',
-  'zh-tw': '載入翻譯',
-}
+  'zh-tw': '載入翻譯'
+};
 
-function SetText() {
-  const locale = localStorage?.getItem('i18nextLng') || 'en'
-  setLoadingText(LOADING_LOCALES[locale.toLowerCase()] || LOADING_LOCALES.en)
-  return <div />
-}
+function SetText () {
+  
+  const locale = localStorage?.getItem('i18nextLng') || 'en';
+  
+  setLoadingText(LOADING_LOCALES[locale.toLowerCase()] || LOADING_LOCALES.en);
+  
+  return <div />;
+  
+};
 
-export function App() {
-  const theme = useCustomTheme()
+export function App () {
+  const theme = useCustomTheme();
 
-  const isValid = isLocalStorageEnabled()
+  const isValid = isLocalStorageEnabled();
 
   if (!isValid) {
-    setLoadingText('Local storage is required to use this app!')
-  }
+    
+    setLoadingText('Local storage is required to use this app!');
+    
+  };
 
   return isValid ? (
     <ThemeProvider theme={theme}>
@@ -68,5 +74,6 @@ export function App() {
         </React.Suspense>
       </ErrorBoundary>
     </ThemeProvider>
-  ) : null
-}
+  ) : null;
+  
+};
