f70ab39b5bdb6a6da09c8b9ea3aac6075ba2ce25
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 19:14:10 +0100

    Updated checkAdvFilter.js

--- src/utils/checkAdvFilter.js
+++ src/utils/checkAdvFilter.js
@@ -7,7 +7,7 @@
  * @param { string } filter
  * @param { boolean } dnf
 **/
-export function checkIVFilterValid( filter, dnf = true ) {
+export function checkIVFilterValid ( filter, dnf = true ) {
   
   const input = filter.toUpperCase();
   const tokenizer = /\s*([()|&!,]|([ADSLXG]?|CP|LC|[GU]L)\s*([0-9]+(?:\.[0-9]*)?)(?:\s*-\s*([0-9]+(?:\.[0-9]*)?))?)/g;
