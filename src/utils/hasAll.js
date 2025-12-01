1cc9e081b9bdd9d382258322453271770e7aaf4c
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 19:39:26 +0100

    Updated hasAll.js

--- src/utils/hasAll.js
+++ src/utils/hasAll.js
@@ -1,22 +1,27 @@
 // @ts-check
 
 /**
- * @param {string} category
- * @param {string} id
- * @returns {boolean}
- */
-export function checkIfHasAll(category, id) {
+ * @param { string } category
+ * @param { string } id
+ * @returns { boolean }
+**/
+export function checkIfHasAll ( category, id ) {
+  
   return (
+    
     category === 'pokemon' ||
-    (category === 'pokestops' &&
+    ( category === 'pokestops' &&
+     
       !(
-        id.startsWith('l') ||
-        id.startsWith('i') ||
-        id.startsWith('f') ||
-        id.startsWith('a') ||
-        id.startsWith('h') ||
-        id.startsWith('b')
+        id.startsWith( 'l' ) ||
+        id.startsWith( 'i' ) ||
+        id.startsWith( 'f' ) ||
+        id.startsWith( 'a' ) ||
+        id.startsWith( 'h' ) ||
+        id.startsWith( 'b' )
       )) ||
-    (id.startsWith('t') && id !== 't0-0')
-  )
-}
+    ( id.startsWith( 't' ) && id !== 't0-0' )
+    
+  );
+  
+};
