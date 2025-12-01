7f8744f78c7830c3d0dd46119fa11ca7c50352c6
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-01 19:29:49 +0100

    Updated getGruntReward.js

--- src/utils/getGruntReward.js
+++ src/utils/getGruntReward.js
@@ -1,26 +1,40 @@
 // @ts-check
 /**
- * @typedef {import('@rm/masterfile').Masterfile['invasions']} Invasions
- * @param {Invasions[keyof Invasions]} grunt
- * @returns {{ first: number, second: number, third: number }}
- */
-export const getGruntReward = (grunt) => {
-  const base = { first: 0, second: 0, third: 0 }
-  if (!grunt || grunt.type.startsWith('NPC')) {
-    return base
-  }
-  if (grunt.secondReward) {
-    base.first = 85
-    base.second = 15
-    return base
-  }
-  if (grunt.thirdReward) {
-    base.third = 100
-    return base
-  }
-  if (grunt.firstReward) {
-    base.first = 100
-    return base
-  }
-  return base
-}
+ * @typedef { import( '@rm/masterfile' ).Masterfile[ 'invasions' ] } Invasions
+ * @param { Invasions[ keyof Invasions ] } grunt
+ * @returns { { first: number, second: number, third: number } }
+**/
+export const getGruntReward = ( grunt ) => {
+  
+  const base = { first: 0, second: 0, third: 0 };
+  
+  if ( !grunt || grunt.type.startsWith( 'NPC' ) ) {
+    
+    return base;
+    
+  };
+  if ( grunt.secondReward ) {
+    
+    base.first = 85;
+    base.second = 15;
+    
+    return base;
+  };
+  if ( grunt.thirdReward ) {
+    
+    base.third = 100;
+    
+    return base;
+    
+  };
+  if ( grunt.firstReward ) {
+    
+    base.first = 100;
+    
+    return base;
+    
+  };
+  
+  return base;
+  
+};
